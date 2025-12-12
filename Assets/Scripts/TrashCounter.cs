c08b187c201dd4d02563cabe33fd669833996aba
Karina26C <avagyankarul26@gmail.com>
2025-12-12 23:13:30 +0400

    Making a trash counter

---
+++ Assets/Scripts/TrashCounter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashCounter : BaseCounter
+{
+    private KitchenObjectSO KitchenObjectSO;
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            player.GetKitchenObject().DestroySelf();
+        }
+    }
+}
